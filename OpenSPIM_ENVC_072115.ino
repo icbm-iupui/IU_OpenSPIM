580aa00e67f401c47bbc2dda790ef5aedcc97c4e
icbm-iupui <winfrees@iu.edu>
2016-12-06 13:53:27 -0500

    Adding SNAPSHOT jar and firmwares
    SNAPSHOT jar supporting the arduino DAC and filterwheel within the plugin, dll for arduino in micromanager (replaces) and three arduino firmwares for the DAC, control of a neutral density filter wheel and a pump controller for media, perfusion and temperature control with software PID.

---
+++ OpenSPIM_ENVC_072115.ino
@@ -0,0 +1,188 @@
+/* Open-Spim Temperature and CO2 Perfusion Control
+ *  
+ *  Seth Winfree and Nathaniel Smith
+ *  
+ *  This code uses the following libraries and reference codes:
+ *    The "Hello World" code from Adafruit's RGB LCD Shield Library
+ *    "DC MotorTest" code from Adafruit's Motor Shield V2 Library
+ *    "singleended" code from Adafruit's ADS1015 Library
+ *    "PID_Basic" and "PID_AdaptiveTunings" code from the Arduino PID Library - Version 1.1.1 by Brett Beauregard
+ *    "Using a Thermistor" code provided by Adafruit at https://learn.adafruit.com/thermistor/using-a-thermistor
+ *    PID parameters were tuned according to the Classic Ziegler-Nichols Rule, which can be found at http://www.mstarlabs.com/control/znrule.html
+ * 
+ */
+
+// Included Libraries
+#include <Wire.h>
+#include <Adafruit_MCP23017.h>
+#include <Adafruit_RGBLCDShield.h>
+#include <Adafruit_MotorShield.h>
+#include <Adafruit_ADS1015.h>
+#include <math.h>
+#include <PID_v1.h>
+
+// Initial Settings and Definitions
+Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
+Adafruit_ADS1015 ads1 = Adafruit_ADS1015(0x48);    
+Adafruit_ADS1015 ads2 = Adafruit_ADS1015(0x49); 
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x60); 
+Adafruit_DCMotor *pump = AFMS.getMotor(1);
+int time = 0;
+double steinhart0, steinhart1;
+double adc0, adc1;
+
+// This definition will be used to define the LCD backlight color
+#define GREEN 0x2
+#define WHITE 0x7
+
+//Define the PID variables we'll be using
+double Setpoint, Input, Output;
+double minOutput = 0;
+double maxOutput = 255;
+
+//Define the PID tuning parameters
+double aggKp=120, aggKi=60, aggKd=15;
+
+//Specify the links and tuning parameters
+PID myPID(&Input, &Output, &Setpoint, aggKp, aggKi, aggKd, DIRECT);
+
+void setup() {
+
+  Serial.begin(9600);
+  ads1.begin();
+  //ads2.begin();
+  lcd.begin(16, 2);
+  lcd.setBacklight(WHITE);
+  Wire.begin();
+  AFMS.begin();
+    
+  pump->run(FORWARD);
+  //Serial.println("CLEARDATA");
+  //Serial.println("LABEL, Timepoint, Time, T-Sample, T-Sample 2, Average Temperature, Error, Motor Setting (75-255)");
+
+  //initialize the PID variables
+  Input = 25;
+  Setpoint = 37.1;
+
+  // Use LCD to allow user to select a setpoint temperature
+  uint8_t buttons = lcd.readButtons();
+  lcd.setCursor(0,0);
+  lcd.print(String("IU OpenSPIM ENVC"));
+  lcd.setCursor(0,1);
+  lcd.print(String("Set T(C):"));
+
+  while (buttons != BUTTON_SELECT) {
+    buttons = lcd.readButtons();
+    lcd.setCursor(10,1);
+    lcd.print(Setpoint);
+    if (buttons & BUTTON_UP) {
+      lcd.setCursor(10,1);
+      Setpoint += .1;
+      lcd.print(Setpoint);
+    }
+    if (buttons & BUTTON_DOWN) {
+      lcd.setCursor(10,1);
+      Setpoint -= .1;
+      lcd.print(Setpoint);
+    }
+    delay(100);
+  }
+  
+  //Setpoint += .05;
+
+  //turn the PID on
+  myPID.SetMode(AUTOMATIC);
+  myPID.SetOutputLimits(minOutput,maxOutput); 
+}
+
+void loop() {
+
+  // Read temperature sensors and calculate resistance
+  adc0 = ads1.readADC_SingleEnded(0);
+  adc1 = ads1.readADC_SingleEnded(1);
+  double resistance0=2000/(1/((.003)*adc0)-.2);
+  double resistance1=2000/(1/((.003)*adc1)-.2);
+  
+  delay(100);
+
+  // Use resistance to calculate a temperature using the Steinhart-Hart B parameter equation method
+  steinhart0 = resistance0/10000;     // (R/Ro)
+  steinhart0 = log(steinhart0);                  // ln(R/Ro)
+  steinhart0 /= 3950;                   // 1/B * ln(R/Ro)
+  steinhart0 += 1.0 /( 25 + 273.15); // + (1/To)
+  steinhart0 = 1.0 / steinhart0;                 // Invert
+  steinhart0 -= 273.15;                         // convert to C
+  steinhart1 = resistance1/10000;     // (R/Ro)
+  steinhart1 = log(steinhart1);                  // ln(R/Ro)
+  steinhart1 /= 3950;                   // 1/B * ln(R/Ro)
+  steinhart1 += 1.0 /( 25 + 273.15); // + (1/To)
+  steinhart1 = 1.0 / steinhart1;                 // Invert
+  steinhart1 -= 273.15;                         // convert to C
+
+  delay(100);
+
+  // Display the temperature of the sample and reservoir on the LCD
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(String("IU OpenSPIM ENVC"));
+  lcd.setCursor(0,1);
+  lcd.print(String("Sample: ")+steinhart0);
+  lcd.setCursor(13 ,1);  
+  lcd.print(String("(C)"));
+  //lcd.print(String("Air: ")+steinhart1);
+
+  delay(1000);
+  
+    lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(String("IU OpenSPIM ENVC"));
+  lcd.setCursor(0,1);
+  lcd.print(String("Resrv.: ")+steinhart1);
+  lcd.setCursor(13 ,1);  
+  lcd.print(String("(C)"));
+
+  // Print the temperature of the sample and reservoir to serial
+  time += 5;
+  //Serial.print(time);
+  //Serial.print(String("     ")+ steinhart0+String("      "));
+  //Serial.print(steinhart1+String("   "));
+
+  delay(100);
+  
+  Input = steinhart0;
+  double Inputswitchpoint = .95*Setpoint;
+    
+  if (Input < Inputswitchpoint) {
+    Output = 255;
+    }
+  
+  // Calculate a PID output for the motor, given the temperature of the sample
+  /*else*/ if (isnan(steinhart0) == 0) {  
+  myPID.SetTunings(aggKp, aggKi, aggKd);
+
+  myPID.Compute();
+
+  // The peristaltic pump doesn't function at low voltages, so this redefines the output to low setting (45) or off (0)
+  if (Output < 1) {
+    Output = 0;
+  }
+  else if (Output < 45) { 
+    Output = 45;
+  }
+    
+  pump->setSpeed(Output); 
+
+  delay(100);
+
+  //Serial.print(Output);
+  //Serial.println();
+
+  //Serial.print("DATA,"); Serial.print("TIME,"); Serial.print(time); Serial.print(","); Serial.print(steinhart0); Serial.print(","); Serial.print(steinhart1); Serial.print(","); Serial.print((steinhart1+steinhart0)/2); Serial.print(","); Serial.print((fabs(steinhart1-steinhart0))/2); Serial.print(","); Serial.println(Output);
+  
+  delay(2500);
+  }
+  else {
+    //Serial.println();
+    delay(4600);
+  }
+}
---
+++ OpenSPIM_NDFW_030315.ino
@@ -0,0 +1,328 @@
+/* 
+This is a test sketch for the Adafruit assembled Motor Shield for Arduino v2
+It won't work with v1.x motor shields! Only for the v2's with built in PWM
+control
+
+For use with the Adafruit Motor Shield v2 
+---->	http://www.adafruit.com/products/1438
+*/
+
+
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include <AFMotor.h>
+#include "utility/Adafruit_PWMServoDriver.h"
+#include <Adafruit_MCP23017.h>
+#include <Adafruit_RGBLCDShield.h>
+#include "Tlc5940.h"
+
+//lighting via TLC5940
+TLC_CHANNEL_TYPE channel;
+
+
+
+
+
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Adafruit_DCMotor *pumpIn = AFMS.getMotor(4);
+Adafruit_DCMotor *pumpOut = AFMS.getMotor(3);
+Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
+
+#define RED 0x1
+#define YELLOW 0x3
+#define GREEN 0x2
+#define TEAL 0x6
+#define BLUE 0x4
+#define VIOLET 0x5
+#define WHITE 0x7
+#define MOVEMENTDELAY 100
+#define LIGHTON 2000
+#define LIGHTOFF 0
+#define NDFW 0
+#define PERF 1
+
+
+int menu = NDFW;
+
+// Connect a stepper motor with 200 steps per revolution (1.8 degree)
+// to motor port #2 (M3 and M4)
+Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 1);
+int homeloop = 0;
+int currentPosition = 0;
+boolean perfusionState = false;
+boolean perfusionForward = true;
+boolean empty = false;
+volatile double sensorValue;
+String positionLabels[] = {"MIR ", "ND5  ", "ND4  ", "ND3  ", "ND2 ", "OPEN"};
+String perfusionLabels[] = {"OFF", "ON FR", "EMPTY ", "ON RV"};
+ 
+ 
+ 
+ 
+
+void setup() {
+  
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println("Stepper test!");
+  Serial.println("Double coil");
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.print("IU OpenSPIM NDFW");
+  
+   
+  lcd.setCursor(0, 1);
+  lcd.print("Starting light...");
+  
+  Tlc.init();
+  lightOn();
+
+  
+  AFMS.begin();  
+    coarseHomeWheel();
+    coarseHomeWheel();
+   
+    setMenu();
+    
+    delay(1000); 
+     
+     
+  //run loop
+}
+
+void loop() {
+ while(homeloop <= 12){
+ readButtons();
+ delay(MOVEMENTDELAY);
+ }
+ goToPosition(0);
+ coarseHomeWheel();
+}
+  
+
+ void coarseHomeWheel(){
+   int lastPosition = currentPosition;
+  lcd.setBacklight(RED);
+  lcd.setCursor(0, 1);
+  lcd.print("Homing wheel...     ");
+  sensorValue = analogRead(A0);
+  myMotor->setSpeed(50);  // 10 rpm   
+    while(sensorValue<=50){
+      lcd.print("Homing wheel...     ");
+      sensorValue = analogRead(A0);
+      delay(100);
+      sensorValue = analogRead(A0);
+      myMotor->step(1, FORWARD, DOUBLE); 
+      sensorValue = analogRead(A0);
+      currentPosition = 0;
+      lcd.setCursor(0,1);
+  }
+  lcd.setBacklight(WHITE);
+  lcd.setCursor(0,1);
+  lcd.print("At Filter: " + positionLabels[currentPosition] + "     ");
+  goToPosition(lastPosition);
+  homeloop = 0;
+ }
+ 
+ void readButtons(){
+   uint8_t buttons = lcd.readButtons();
+   int selection = 0;
+   
+
+  if (buttons) {
+    lcd.clear();
+    lcd.setCursor(0,0);
+    if(menu == NDFW){
+      if (buttons & BUTTON_UP) {
+      lightOn();
+      setMenu();
+    }
+    if (buttons & BUTTON_DOWN) {
+      lightOff();
+      setMenu();
+    }
+    if (buttons & BUTTON_LEFT) {
+      setMenu();
+      lcd.setCursor(0,1);
+      moveBackward(true);
+      lcd.setBacklight(WHITE);
+    } 
+    if (buttons & BUTTON_RIGHT) {
+      setMenu();
+      lcd.setCursor(0,1);
+      moveForward(true);
+      lcd.setBacklight(WHITE);
+    }
+    }
+    if(menu == PERF){
+    boolean b = !perfusionState;
+    if (buttons & BUTTON_LEFT){
+      stopPumps();
+      setMenu();
+    }
+    if (buttons & BUTTON_RIGHT) {
+      startPumps();
+      setMenu();
+    } 
+    if (buttons & BUTTON_DOWN) {
+      emptyChamber();
+      empty = true;
+      setMenu();
+    }
+    if (buttons & BUTTON_UP) {
+      startPumpsReverse();
+      setMenu();
+    }
+    }
+    }
+    if (buttons & BUTTON_SELECT) {
+      if(menu == NDFW){menu = PERF;}
+      else{menu = NDFW;}
+      setMenu();
+    }
+ }
+ 
+
+//Setup LCD menu
+ void setMenu(){
+   if(menu == NDFW){
+   lcd.clear();
+   lcd.setCursor(0, 0);
+   lcd.setBacklight(WHITE);
+   lcd.print("IU OpenSPIM NDFW");
+   lcd.setCursor(0, 1);
+    lcd.print("At Filter: " + positionLabels[currentPosition] + "     ");
+   }
+   else{
+   lcd.clear();
+   lcd.setCursor(0, 0);
+   lcd.setBacklight(WHITE);
+   lcd.print("IU OpenSPIM PERF");
+   lcd.setCursor(0, 1);
+   if(perfusionState){
+     if(perfusionForward){
+   lcd.print("Perfusion: " + perfusionLabels[1] + "     ");
+     }
+     else{
+       lcd.print("Perfusion: " + perfusionLabels[3] + "     ");
+     }
+   }else if(empty){
+     lcd.print("Perfusion: " + perfusionLabels[2] + "     ");
+    
+   }else{
+     lcd.print("Perfusion: " + perfusionLabels[0] + "     ");
+   }
+   }
+ 
+ }
+ 
+ //move forward
+ void moveForward(boolean count){
+ myMotor->setSpeed(100); 
+ myMotor->step(33, FORWARD, DOUBLE); 
+ 
+ if(currentPosition == 5){
+   currentPosition = 0;
+ }
+ else {
+   currentPosition++;
+ }
+ lcd.print("At Filter: " + positionLabels[currentPosition] + "     ");
+ if(count == true){homeloop++;}
+ }
+ 
+ //move backward
+ void moveBackward(boolean count){
+  myMotor->setSpeed(100); 
+ myMotor->step(33, BACKWARD, DOUBLE); 
+  
+  
+ if(currentPosition == 0){
+   currentPosition = 5;
+ }
+ else {
+   currentPosition--;
+ }
+ lcd.print("At Filter: " + positionLabels[currentPosition] + "     ");
+ if(count == true){homeloop++;}
+ }
+ 
+ //goto select position
+ boolean goToPosition(int position){     
+       while(currentPosition != position){
+         lcd.setBacklight(RED);
+         lcd.setCursor(0,1);
+         moveForward(false);
+       }  
+ return true;      
+ }
+ 
+ //turn BL off
+ 
+ void lightOff(){
+   for(int i = 0; i <= 20; i++){   
+     Tlc.set(i, LIGHTOFF); 
+     delay(5); 
+     Tlc.update();    
+  }
+ }
+ 
+ //turn BL on
+
+ void lightOn(){ 
+   for(int i = 0; i <= 20; i++){   
+     Tlc.set(i, LIGHTON); 
+     delay(5); 
+     Tlc.update();    
+  }
+ }
+ 
+ //start perfusion pumps
+ 
+ void startPumps(){
+ 
+ pumpIn->setSpeed(100); 
+ pumpOut->setSpeed(115);
+ pumpIn->run(FORWARD);
+ pumpOut->run(BACKWARD);
+ perfusionState = true;
+ perfusionForward = true;
+ empty = false;
+ }
+ 
+  //stop perfusion pumps
+ 
+ void stopPumps(){
+ 
+pumpIn->setSpeed(0);
+ pumpOut->setSpeed(0);
+ pumpIn->run(RELEASE);
+ pumpOut->run(RELEASE);
+ perfusionState = false;
+ empty = false;
+ }
+ 
+  void startPumpsReverse(){
+ 
+ pumpIn->setSpeed(255); 
+ pumpOut->setSpeed(200);
+ pumpIn->run(BACKWARD);
+ pumpOut->run(FORWARD);
+ perfusionState = true;
+ perfusionForward = false;
+ empty = false;
+ }
+ 
+ void emptyChamber(){
+  pumpIn->setSpeed(0); 
+  pumpOut->setSpeed(0); 
+  pumpOut->run(RELEASE); 
+  pumpIn->run(BACKWARD);
+  pumpIn->setSpeed(65000);  
+  perfusionState = false;
+  empty = true;
+ }
+ 
+
+ 
+
